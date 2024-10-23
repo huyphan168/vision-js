e1482d20e6e13dba11bc9f9dc2bc48b0801f6ff9
huyphan168 <huypn168@gmail.com>
2024-10-23 22:49:01 +0000

    small updates

--- frontend/.next/server/pages/people.js
+++
@@ -1,5 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/pages/people.js";
-const runtime = require("../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_1379a0._.js");
-runtime.loadChunk("server/chunks/ssr/[root of the server]__7f3687._.js");
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/src/pages/people.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/src/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
